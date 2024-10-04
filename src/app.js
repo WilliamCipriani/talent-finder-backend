bc976c452713731bd966302673e2afedc7530022
WilliamCipriani <wciprianinaveda@gmail.com>
2024-10-04 15:46:24 -0500

    update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://talentfinder1.vercel.app/', // Permitir solicitudes desde este origen
+  origin: 'https://talentfinder1.vercel.app', // Permitir solicitudes desde este origen
   //origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
